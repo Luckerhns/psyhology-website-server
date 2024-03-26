a78a159b9d7ef338ff808a79771c2db89aaa0643
Max <mamedov@Maxs-Laptop.local>
2024-03-27 01:55:28 +0300

    fixed-3

--- api/services/dependencies/Activate.service.ts
+++ api/services/dependencies/Activate.service.ts
@@ -2,10 +2,10 @@ import User from "../../../database/models/User";
 import ErrorException from "../../errors/ErrorException";
 
 export default class ActivateService {
-    public static async activate(link) {
-        const user = await User.findOne({where: {activationLink: link}})
-        if(!user) throw ErrorException.BadRequest('Incorrect activation link')
+  public static async activate(link) {
+    const user = await User.findOne({ where: { activationLink: link } });
+    if (!user) throw ErrorException.BadRequest("Incorrect activation link");
 
-        await user.update({isActivated: true})
-    }
+    await user.update({ isActivated: true });
+  }
 }
